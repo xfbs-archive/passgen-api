2394d4774415c2eedfbf4c8a81e6d235228629ae
Patrick Elsen <pelsen@xfbs.net>
2019-08-13 00:37:23 +0200

    Cleans up example code

--- src/api_server.c
+++ src/api_server.c
@@ -1,18 +1,16 @@
-/*
- * Copyright (c) 2014 Cesanta Software Limited
- * All rights reserved
- */
-
-#include "db_plugin.h"
+#include <mongoose.h>
+#include <passgen/version.h>
+#include <jansson.h>
 
 static const char *s_http_port = "8000";
-static struct mg_serve_http_opts s_http_server_opts;
 static int s_sig_num = 0;
-static void *s_db_handle = NULL;
-static const char *s_db_path = "api_server.db";
-static const struct mg_str s_get_method = MG_MK_STR("GET");
-static const struct mg_str s_put_method = MG_MK_STR("PUT");
-static const struct mg_str s_delele_method = MG_MK_STR("DELETE");
+
+typedef void handle_method(struct mg_connection *conn, const struct http_message *msg);
+struct route {
+  struct mg_str path;
+  struct mg_str method;
+  handle_method *handler;
+};
 
 static void signal_handler(int sig_num) {
   signal(sig_num, signal_handler);
@@ -20,36 +18,41 @@ static void signal_handler(int sig_num) {
 }
 
 static int has_prefix(const struct mg_str *uri, const struct mg_str *prefix) {
-  return uri->len > prefix->len && memcmp(uri->p, prefix->p, prefix->len) == 0;
+  return uri->len >= prefix->len && memcmp(uri->p, prefix->p, prefix->len) == 0;
 }
 
 static int is_equal(const struct mg_str *s1, const struct mg_str *s2) {
   return s1->len == s2->len && memcmp(s1->p, s2->p, s2->len) == 0;
 }
 
+void handle_version(struct mg_connection *nc, const struct http_message *hm);
+void handle_passgen(struct mg_connection *nc, const struct http_message *hm);
+void handle_version_json(struct mg_connection *nc, const struct http_message *hm);
+
 static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
-  static const struct mg_str api_prefix = MG_MK_STR("/api/v1");
+  static const struct route routes[] = {
+    {MG_MK_STR("/api/v1/version.json"), MG_MK_STR("GET"), &handle_version_json},
+    {MG_MK_STR("/api/v1/version"), MG_MK_STR("GET"), &handle_version},
+    {MG_MK_STR("/api/v1/passgen"), MG_MK_STR("GET"), &handle_passgen},
+  };
   struct http_message *hm = (struct http_message *) ev_data;
-  struct mg_str key;
+  bool found = false;
 
   switch (ev) {
     case MG_EV_HTTP_REQUEST:
-      if (has_prefix(&hm->uri, &api_prefix)) {
-        key.p = hm->uri.p + api_prefix.len;
-        key.len = hm->uri.len - api_prefix.len;
-        if (is_equal(&hm->method, &s_get_method)) {
-          db_op(nc, hm, &key, s_db_handle, API_OP_GET);
-        } else if (is_equal(&hm->method, &s_put_method)) {
-          db_op(nc, hm, &key, s_db_handle, API_OP_SET);
-        } else if (is_equal(&hm->method, &s_delele_method)) {
-          db_op(nc, hm, &key, s_db_handle, API_OP_DEL);
-        } else {
-          mg_printf(nc, "%s",
-                    "HTTP/1.0 501 Not Implemented\r\n"
-                    "Content-Length: 0\r\n\r\n");
+      for(size_t i = 0; i < sizeof(routes); i++) {
+        if(is_equal(&hm->method, &routes[i].method) &&
+            has_prefix(&hm->uri, &routes[i].path)) {
+          routes[i].handler(nc, hm);
+          found = true;
+          break;
         }
-      } else {
-        mg_serve_http(nc, hm, s_http_server_opts); /* Serve static content */
+      }
+
+      if(!found) {
+        mg_printf(nc,
+          "HTTP/1.0 404 Not Found\r\n"
+          "Content-Length: 0\r\n\r\n");
       }
       break;
     default:
@@ -57,36 +60,67 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
   }
 }
 
+void handle_version(struct mg_connection *nc, const struct http_message *hm) {
+  const char *data = passgen_version_str();
+
+  mg_printf(nc,
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/plain\r\n"
+    "Content-Length: %d\r\n\r\n%s",
+    (int) strlen(data), data);
+}
+
+void handle_passgen(struct mg_connection *nc, const struct http_message *hm) {
+  char format[256];
+  int ret = mg_get_http_var(&hm->query_string, "format", format, sizeof(format));
+
+  if(ret > 0) {
+    mg_printf(nc,
+      "HTTP/1.1 200 OK\r\n"
+      "Content-Type: text/plain\r\n"
+      "Content-Length: %d\r\n\r\n%s",
+      ret, format);
+  } else {
+    mg_printf(nc,
+      "HTTP/1.0 413 Request Entity Too Large\r\n"
+      "Content-Length: 0\r\n\r\n");
+  }
+}
+
+void handle_version_json(struct mg_connection *nc, const struct http_message *hm) {
+  const char *version_str = passgen_version_str();
+  passgen_version_t version = passgen_version();
+
+  json_t *body = json_object();
+  json_object_set_new(body, "version", json_string_nocheck(version_str));
+  json_object_set_new(body, "major", json_integer(version.maj));
+  json_object_set_new(body, "minor", json_integer(version.min));
+  json_object_set_new(body, "revision", json_integer(version.rev));
+
+  char *output = json_dumps(body, JSON_COMPACT);
+  size_t output_len = strlen(output);
+
+  mg_printf(nc,
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/plain\r\n"
+    "Content-Length: %zu\r\n\r\n%s",
+    output_len, output);
+
+  free(output);
+  json_decref(body);
+}
+
 int main(int argc, char *argv[]) {
   struct mg_mgr mgr;
   struct mg_connection *nc;
-  int i;
 
   /* Open listening socket */
   mg_mgr_init(&mgr, NULL);
   nc = mg_bind(&mgr, s_http_port, ev_handler);
   mg_set_protocol_http_websocket(nc);
-  s_http_server_opts.document_root = "web_root";
-
-  /* Parse command line arguments */
-  for (i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "-D") == 0) {
-      mgr.hexdump_file = argv[++i];
-    } else if (strcmp(argv[i], "-f") == 0) {
-      s_db_path = argv[++i];
-    } else if (strcmp(argv[i], "-r") == 0) {
-      s_http_server_opts.document_root = argv[++i];
-    }
-  }
 
   signal(SIGINT, signal_handler);
   signal(SIGTERM, signal_handler);
-
-  /* Open database */
-  if ((s_db_handle = db_open(s_db_path)) == NULL) {
-    fprintf(stderr, "Cannot open DB [%s]\n", s_db_path);
-    exit(EXIT_FAILURE);
-  }
 
   /* Run event loop until signal is received */
   printf("Starting RESTful server on port %s\n", s_http_port);
@@ -96,7 +130,6 @@ int main(int argc, char *argv[]) {
 
   /* Cleanup */
   mg_mgr_free(&mgr);
-  db_close(&s_db_handle);
 
   printf("Exiting on signal %d\n", s_sig_num);
 
